1065b6a85b8054c32105057a6518c69295d1e1ea
SearchCode00 <SearchCode00@users.noreply.github.com>
2017-07-11 10:45:41 +0545

    Update Person.java

--- Inheritance/Person.java
+++ Inheritance/Person.java
@@ -6,7 +6,15 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+    
+    public String getFirstName(){
+        return firstName;
+    }
 
+    public String getLastName(){
+        return lastName;
+    }
+    
     public void sayHello() {
         System.out.println("Hello " + firstName + " " + lastName);
     }
